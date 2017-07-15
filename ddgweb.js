067f5c4fcbf7ef8d6109c55342e25aaa393a0828
Allen B <pingguo.ny+git@gmail.com>
2017-07-14 21:53:11 -0400

    Update ddgweb.js

--- ddgweb.js
+++ ddgweb.js
@@ -5,7 +5,7 @@ ddg.rawApi = function(query, ssl) {
     var name = "ddg_rawApi_" + now + "" + Math.floor(Math.random() * 1000 + 1);
     window[name] = resolve;
     var script = document.createElement("script");
-    script.src = "http" + (ssl ? "s": "") + "://api.duckduckgo.com/?q=" + encodeURI(query);
+    script.src = "http" + (ssl ? "s": "") + "://api.duckduckgo.com/?q=" + encodeURI(query) + "&format=json&pretty=0&callback=" + name;
     document.getElementsByTagName("head")[0].appendChild(script);
   });
 }
